1b60ddcff270d9ec56e2101aeb724cbb5888d3a5
Chris.Franklin <cfrank306@gmail.com>
2021-11-09 23:39:59 -0600

    fixed homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,26 +2,22 @@ const router = require('express').Router();
 const { User, Comment, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+// Display all posts for everyone, no Auth required.
+router.get('/', async (req, res) => {
     try {
-    const userData = await User.findAll({
-        attributes: { exclude: ['password'] },
-        order: [['name', 'ASC']],
+    const postData = await Post.findAll({
+        include: [{ model: User, attributes: ['id', 'name']}],
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const currentPosts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', {
-        users,
-      // Pass the logged in flag to the template
-        logged_in: req.session.logged_in,
-    });
+    res.render('homepage', { currentPosts, logged_in: req.session.logged_in,});
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
+// Login route rendering
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
     if (req.session.logged_in) {
@@ -30,6 +26,15 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+// Create New route rendering
+router.get('/createNew', (req, res) => {
+    if(req.session.logged_in){
+        res.redirect('/');
+        return;
+    }
+    res.render('createNew');
 });
 
 module.exports = router;
